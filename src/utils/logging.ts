56010129fbaa986842f9f397cada33577ae76a68
Ashley Smith <ashley.smith@thetradedesk.com>
2024-11-08 13:01:52 -0700

    format info

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -1,7 +1,21 @@
 import expressWinston from 'express-winston';
-import winston, { createLogger } from 'winston';
+import winston, { createLogger, format } from 'winston';
 
 import { isProduction } from './process';
+
+function truncateMessage(message: string, maxChars: number): string {
+  return message.length > maxChars ? message.substring(0, maxChars) : message;
+}
+
+const formatInfo = format((info) => {
+  if (info.private) { return false; }
+  info.message = truncateMessage(info.message, 250);
+  if (info.meta) {
+    const {meta, ...rest} = info;
+   return rest;
+  }
+  return info;
+});
 
 const logger = createLogger({
   transports: [
@@ -9,7 +23,12 @@ const logger = createLogger({
       level: isProduction ? 'info' : 'debug',
     }),
   ],
+  format: format.combine(
+    formatInfo(),
+    format.json(),
+  ),
 });
+
 logger.exceptions.handle(new winston.transports.Console({
   level: isProduction ? 'info' : 'debug',
 }));
